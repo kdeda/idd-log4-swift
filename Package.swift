1aeafa7addd497ec27651927dca5f518687eb2f2
Klajd Deda <kdeda@id-design.com>
2022-05-22 17:56:14 -0400

    Adding support for ios13

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "idd-log4-swift",
     platforms: [
-//      .iOS(.v13),
-      .macOS(.v10_12)
+        .iOS(.v13),
+        .macOS(.v10_12)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
