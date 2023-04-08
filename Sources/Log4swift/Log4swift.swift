0db2a4cf81631bdff5dbdee445562686e6dab750
Klajd Deda <kdeda@id-design.com>
2023-04-08 14:40:13 -0400

    Added config for convenience

--- Sources/Log4swift/Log4swift.swift
+++ Sources/Log4swift/Log4swift.swift
@@ -61,6 +61,37 @@ public final class Log4swift {
     }
 
     /**
+     If you define the -standardLog true at the start of your application, all
+     log lessages will go to the Console (under stdout).
+     Otherwise if the app is running in user space they will go to ~/Library/Logs/${appName}.log
+     but if the app is running as root the logs will go to /Library/Logs/${appName}.log
+     
+     Any extra config has to be done manually.
+     */
+    static public func configure(appName: String) {
+        if UserDefaults.standard.bool(forKey: "standardLog") {
+            LoggingSystem.bootstrap { label in
+                ConsoleHandler(label: label)
+            }
+        } else {
+            let home: URL = {
+                let uid = getuid()
+                guard uid != 0
+                else { return URL.init(fileURLWithPath: "/") }
+                
+                return URL.init(fileURLWithPath: NSHomeDirectory())
+            }()
+            
+            let fileURL = home.appendingPathComponent("Library/Logs").appendingPathComponent(appName).appendingPathExtension("log")
+            let fileLogger = try? FileLogging(to: fileURL)
+            
+            LoggingSystem.bootstrap { label in
+                FileLogHandler(label: label, fileLogger: fileLogger!)
+            }
+        }
+    }
+    
+    /**
      This is very fast, 3 seconds for 1 million look ups
      */
     static public subscript(identifier: String) -> Logger {
