6d3b983f5441e76b32990054eb1b26337acdccf8
Klajd Deda <kdeda@id-design.com>
2023-10-18 09:12:39 -0400

    * Convenience to manually pass in the func name

---
+++ Sources/Log4swift/Logger+Extensions.swift
@@ -0,0 +1,40 @@
+//
+//  Logger.swift
+//  Log4swift
+//
+//  Created by Klajd Deda on 3/9/23.
+//  Copyright (C) 1997-2023 id-design, inc. All rights reserved.
+//
+
+import Foundation
+import Logging
+
+extension Logging.Logger {
+    public var isDebug: Bool {
+        self.logLevel == .debug
+    }
+
+    @inlinable
+    /**
+     Convenience to log the function name, usually inside a client.
+     */
+    public func info(function: String, _ message: @autoclosure () -> Logger.Message) {
+        self.log(level: .info, message(), metadata: nil, source: nil, file: #file, function: function, line: #line)
+    }
+    
+    @inlinable
+    /**
+     Convenience to log the function name, usually inside a client.
+     */
+    public func debug(function: String, _ message: @autoclosure () -> Logger.Message) {
+        self.log(level: .debug, message(), metadata: nil, source: nil, file: #file, function: function, line: #line)
+    }
+
+    @inlinable
+    /**
+     Convenience to log the function name, usually inside a client.
+     */
+    public func error(function: String, _ message: @autoclosure () -> Logger.Message) {
+        self.log(level: .error, message(), metadata: nil, source: nil, file: #file, function: function, line: #line)
+    }
+}
